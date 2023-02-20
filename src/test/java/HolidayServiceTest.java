efa7bd5e1cd2296dd5714b6ce8204f5fdc41be05
Helen89 <mulen_rush89@mail.ru>
2023-02-20 10:42:41 +0700

    ParameterizedTest

--- src/test/java/HolidayServiceTest.java
+++ src/test/java/HolidayServiceTest.java
@@ -1,21 +1,16 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HolidayServiceTest {
-    @Test
-    void shouldCalculate1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/holiday.csv")
+    public void shouldCalculate1(int expected, int income, int expenses, int threshold) {
         HolidayService service = new HolidayService();
 
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        Assertions.assertEquals(expected, actual);
-    }
-    @Test
-    void shouldCalculate2() {
-        HolidayService service = new HolidayService();
-
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 }
